7c1c443dbfa566b46d5a6f6285d8b1d61410a624
Jens Küsters <jens@kuesters.net>
2025-06-22 12:42:32 +0200

    Add images and update components to use Picture for better performance
    - Added new images: wackelzug2018.jpg and zug.jpg to assets.
    - Updated Quote.astro to use Picture component for wackelzug2018.jpg.
    - Updated Statistics.astro to use Picture component for flagge.jpg.
    - Updated Layout.astro to use Picture component for zug.jpg and replaced img tag for logo with Logo component.
    - Removed background image CSS variables for flagge.jpg and wackelzug2018.jpg from main.css.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    responsiveStyles: true,
+  },
   vite: {
     ssr: {
         external: ['svgo'],
